c3e886be5f78d8bdb7c7d0498e35654e4753c958
manavtore <toremanav@gmail.com>
2023-12-05 03:19:31 +0530

    fix(contract): update version

--- Backend/Contracts/Fees_management.sol
+++ Backend/Contracts/Fees_management.sol
@@ -1,73 +1,62 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity <=0.8.19;
+pragma solidity ^0.8.0;
 
-contract Management
-{
-
-struct Admin{
-    address admin;
-    string name;
-}
-
-
-
-struct Student{
-    address student;
-    string name;
-    uint256 totalfees_fees;
-    uint256 feesPaid;
-    uint256 feesDue;
-}
-
-Admin[] public admin;
-Student[] public student;
-
-
-
-modifier onlyAdmin(){
-    bool isAdmin = false;
-    for(uint256 i=0; i<admin.length; i++){
-        if(admin[i].admin == msg.sender){
-            isAdmin = true;
-        }
+contract Management {
+    struct Admin {
+        address admin;
+        string name;
     }
-    require(isAdmin == true, "You are not an admin");
-    _;
-}
 
-
-
-function addAdmin(address _admin, string memory _name) public{
-    admin.push(Admin(_admin, _name));
-}
-
-
-function addStudent(address _student, string memory _name, uint256 _totalfees_fees) onlyAdmin public{
-    student.push(Student(_student, _name, _totalfees_fees, 0, _totalfees_fees));
-}
-
-
-function payFees(address _student, uint256 _feesPaid) public payable{
-    for(uint256 i=0; i<student.length; i++){
-        if(student[i].student == _student){
-            student[i].feesPaid += _feesPaid;
-            student[i].feesDue -= _feesPaid;
-        }
+    struct Student {
+        address student;
+        string name;
+        uint256 totalFees;
+        uint256 feesPaid;
+        uint256 feesDue;
     }
-}
 
-  function checkFees(address _student) public view returns(uint256 _feespaid, uint256 _feesdue){
-        for(uint256 i=0; i<student.length; i++){
-            if(student[i].student == _student){
-                return(student[i].feesPaid, student[i].feesDue);
+    Admin[] public admins;
+    Student[] public students;
+
+    mapping(address => bool) public isAdmin;
+
+    modifier onlyAdmin() {
+        require(isAdmin[msg.sender], "You are not an admin");
+        _;
+    }
+
+    modifier onlyStudent(address _student) {
+        require(msg.sender == _student, "You are not the authorized student");
+        _;
+    }
+
+    function addAdmin(address _admin, string memory _name) public {
+        admins.push(Admin(_admin, _name));
+        isAdmin[_admin] = true;
+    }
+
+    function addStudent(address _student, string memory _name, uint256 _totalFees) onlyAdmin public {
+        students.push(Student(_student, _name, _totalFees, 0, _totalFees));
+    }
+
+    function payFees(uint256 _feesPaid) onlyStudent(msg.sender) public payable {
+        for (uint256 i = 0; i < students.length; i++) {
+            if (students[i].student == msg.sender) {
+                require(students[i].feesDue >= _feesPaid, "Amount exceeds remaining fees");
+                students[i].feesPaid += _feesPaid;
+                students[i].feesDue -= _feesPaid;
             }
-        
+        }
     }
 
-    
-
-
-
-}
+    function checkFees() public view returns (uint256 feesPaid, uint256 feesDue) {
+        for (uint256 i = 0; i < students.length; i++) {
+            if (students[i].student == msg.sender) {
+                feesPaid = students[i].feesPaid;
+                feesDue = students[i].feesDue;
+                return (feesPaid, feesDue);
+            }
+        }
+    }
 }
